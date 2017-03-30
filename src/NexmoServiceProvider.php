04e3d1799b323c84419da1c1146504fdb90871a9
Percy Mamedy <percymamedy@gmail.com>
2017-03-30 22:18:17 +0400

    Refacto and enhancement
    Added .gitignore, .gitattributes plus made some changes to the NexmoServiceProvider and Facade
    so as they reflect more the Laravel coding style.

--- src/NexmoServiceProvider.php
+++ src/NexmoServiceProvider.php
@@ -2,52 +2,101 @@
 
 namespace Nexmo\Laravel;
 
-use Illuminate\Support\ServiceProvider;
 use Nexmo\Client;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Config\Repository as Config;
 
 class NexmoServiceProvider extends ServiceProvider
 {
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
     protected $defer = true;
 
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
     public function boot()
     {
+        // Config file path.
         $dist = __DIR__.'/../config/nexmo.php';
+
+        // Publishes config File.
         $this->publishes([
             $dist => config_path('nexmo.php'),
         ]);
 
+        // Merge config.
         $this->mergeConfigFrom($dist, 'nexmo');
     }
 
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
     public function register()
     {
-        $this->app->singleton(Client::class, function($app){
-            $config = $app['config']->get('nexmo');
-
-            if(!$config){
-                throw new \RuntimeException('missing nexmo configuration section');
-            }
-
-            if(!isset($config['api_key'])){
-                throw new \RuntimeException('missing nexmo configuration: `api_key`');
-            }
-
-            if(isset($config['api_secret'])){
-                $credentials = new Client\Credentials\Basic($config['api_key'], $config['api_secret']);
-            } elseif(isset($config['signature_secret'])) {
-                $credentials = new Client\Credentials\SharedSecret($config['api_key'], $config['signature_secret']);
-            }
-
-            if(!isset($credentials)){
-                throw new \RuntimeException('missing nexmo configuration: `api_secret` or `signature_secret`');
-            }
-
-            $options = array_diff_key($config, ['api_key', 'api_secret', 'shared_secret']);
-
-            return new Client($credentials, $options);
+        // Bind Nexmo Client in Service Container.
+        $this->app->singleton(Client::class, function ($app) {
+            return $this->createNexmoClient($app['config']);
         });
     }
 
+    /**
+     * Create a new Nexmo Client.
+     *
+     * @param Config $config
+     *
+     * @return Client;
+     */
+    protected function createNexmoClient(Config $config)
+    {
+        // Check for Nexmo config file.
+        if (! $config->has('nexmo')) {
+            throw new \RuntimeException('missing nexmo configuration section');
+        }
+
+        // Check for API_KEY.
+        if (! $config->has('nexmo.api_key')) {
+            throw new \RuntimeException('missing nexmo configuration: `api_key`');
+        }
+
+        // Check whether config is setup
+        // for using API_SECRET
+        // otherwise use
+        // SIGNATURE
+        if ($config->has('nexmo.api_secret')) {
+            // Create Basic Credentials.
+            $credentials = new Client\Credentials\Basic(
+                $config->get('nexmo.api_key'), $config->get('nexmo.api_secret')
+            );
+        } elseif ($config->has('nexmo.signature_secret')) {
+            // Create SharedSecret Credentials.
+            $credentials = new Client\Credentials\SharedSecret(
+                $config->get('nexmo.api_key'), $config->get('nexmo.signature_secret')
+            );
+        }
+
+        if (! isset($credentials)) {
+            throw new \RuntimeException('missing nexmo configuration: `api_secret` or `signature_secret`');
+        }
+
+        // Get Client Options.
+        $options = array_diff_key($config->get('nexmo'), ['api_key', 'api_secret', 'shared_secret']);
+
+        return new Client($credentials, $options);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
     public function provides()
     {
         return [Client::class];
